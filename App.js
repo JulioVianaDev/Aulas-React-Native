6fbe85062eec1904669a91788d445d4b1c192e9d
“juliovianadev” <juliovianadev@gmail.com>
2022-08-21 15:29:31 -0300

    limpando a branch

--- App.js
+++ App.js
@@ -5,25 +5,7 @@ import luffy from './imagens/luffy.png'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá meu primeiro app</Text>
-      <View>
-        <Text>Olá tudo bem?</Text>
-      </View>
-      <View>
-        <Button title="Go to my github page"
-                onPress={()=>{Linking.openURL('https://github.com/JulioVianaDev')}}
-        />
-      </View>
-      <View>
-        <Image source={logo} style={{width:250, height: 250}}></Image>
-      </View>
-      <View>
-        <Text>Ver animes</Text>
-        <Button title="ir para one piece" 
-                onPress={()=>{Linking.openURL('https:onepiece')}}
-        />
-        <Image source={luffy} style={{width:250, height: 250}}/>
-      </View>
+      
     </View>
   );
 }
